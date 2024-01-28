01dbf67c2b93f43d2e0f0613c2e8b457d416bec2
kimju123 <zkfltm7777@gmail.com>
2024-01-29 01:59:09 +0900

    first

---
+++ src/main/reactjs/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import icon from './image/c1.png'
+
+function App() {
+  return (
+    <div className="App">
+      <img alt='' src={icon} />
+      <h3>배포성공 기원</h3>
+    </div>
+  );
+}
+
+export default App;
